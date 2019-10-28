d50ac9b859ad4328cb3a7af9c8cdc52615facc99
dezhizhang <1541609448@qq.com>
2019-10-28 22:30:40 +0800

    组件

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(new Center(
-    child: Text(
-      '你好flutter',
-      textDirection: TextDirection.ltr,
-    ),
-  ));
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('flutter demo'),
+        ),
+        body: HomeContent(),
+      ),
+    );
+  }
+  
+}
+
+
+class HomeContent extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: Text(
+        '哈哈哈你好呀',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+          
+          color: Colors.red
+        ),
+      ),
+    );
+  }
+  
 }
