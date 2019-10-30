078e382786a78ee3b80ee8714208329ef6c4c716
dezhizhang <1541609448@qq.com>
2019-10-31 00:08:35 +0800

    提交

--- lib/main.dart
+++ lib/main.dart
@@ -23,24 +23,52 @@ class MyApp extends StatelessWidget{
   
 }
 
-class HomeContent extends StatelessWidget{
-  List list = ['123','345','789','123'];
-  List<Widget> _getData() {
-    return list.map((value){
-      return ListTile(
-        title: Text(value),
-      );
-    }).toList();
+//  class HomeContent extends StatelessWidget{
+//   List list = [];
+//   HomeContent() {
+//     for(var i=0;i<20;i++) {
+//       list.add('我是第$i条数据');
+//     }
+//   }
+//   @override
+//   Widget build(BuildContext context) {
+//     // TODO: implement build
+//     return Container(
+//       child: ListView.builder(
+//         itemCount: this.list.length,
+//         itemBuilder: (context,index) {
+//           return ListTile(
+//             title: Text(this.list[index]),
+//           );
+//         },
+//       ),
+//     );
+//   }
+   
+//  }
 
+class HomeContent extends StatelessWidget{
+  List list = [];
+  HomeContent() {
+    for(var i=0;i<30;i++) {
+      list.add('我是第$i条数据');
+    }
+    
   }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Container(
-      child: ListView(
-        children: this._getData(),
+      child: ListView.builder(
+        itemCount: this.list.length,
+        itemBuilder: (context,index) {
+          return ListTile(
+            title: Text(this.list[index]),
+          );
+        },
       ),
     );
   }
-  
+
 }
