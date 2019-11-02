d29d426a4a95abc3997172146f4b967a553393b6
dezhizhang <1541609448@qq.com>
2019-11-02 15:40:54 +0800

    图文列表

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,29 @@ class MyApp extends StatelessWidget{
   }
 }
 
+
+// class HomeContent extends StatelessWidget{
+//   @override
+//   Widget build(BuildContext context) {
+//     // TODO: implement build
+//     return ListView(
+//       children: <Widget>[
+//         Card(
+//           child: Column(
+//             children: <Widget>[
+//               ListTile(
+//                 title: Text('高级工程师'),
+//                 subtitle: Text('哈呈哈'),
+//               )
+//             ],
+//           ),
+//         )
+//       ],
+//     );
+//   }
+  
+// }
+
 class HomeContent extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
@@ -31,9 +54,14 @@ class HomeContent extends StatelessWidget{
         Card(
           child: Column(
             children: <Widget>[
+              AspectRatio(
+                aspectRatio: 16 / 9,
+                child: Image.network('https://www.itying.com/images/flutter/3.png',fit: BoxFit.cover),
+              ),
               ListTile(
-                title: Text('张三',style: TextStyle(fontSize: 30)),
-                subtitle: Text('高级工程师'),
+                leading: Icon(Icons.home),
+                title: Text('标题'),
+                subtitle: Text('副标题'),
               )
             ],
           ),
