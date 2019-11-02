b226f2fb93ff53263927e21b49dde65647696a12
dezhizhang <1541609448@qq.com>
2019-11-02 18:12:19 +0800

    tabs

--- lib/main.dart
+++ lib/main.dart
@@ -5,19 +5,60 @@ void main() {
   runApp(MyApp());
 }
 
+
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('首页'),
-        ),
-        body: HomeContent(),
-      ),
+      home: Tabs(),
+     
       theme: ThemeData(
         primaryColor: Colors.pink
+      ),
+    );
+  }
+}
+
+
+
+class Tabs extends StatefulWidget {
+  Tabs({Key key}) : super(key: key);
+
+  @override
+  _TabsState createState() => _TabsState();
+}
+
+class _TabsState extends State<Tabs> {
+  int currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('首页'),
+      ),
+      body: HomeContent(),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: this.currentIndex,
+        onTap: (int index) {
+          setState(() {
+           this.currentIndex = index; 
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('首页')
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            title: Text('分类'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            title: Text('查找')
+          )
+        ],
       ),
     );
   }
@@ -31,25 +72,13 @@ class HomeContent extends StatefulWidget {
 }
 
 class _HomeContentState extends State<HomeContent> {
-  int number = 0;
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Chip(
-          label: Text('${this.number}'),
-        ),
-        RaisedButton(
-          child: Text('增加'),
-          color: Colors.pink,
-          textColor: Colors.white,
-          onPressed: () {
-            setState(() {
-             this.number++; 
-            });
-          },
-        )
+        Text('hello')
       ],
     );
   }
 }
+
