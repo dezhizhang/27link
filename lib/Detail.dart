b789a8af33f409e36bd25efadad1be12badd9056
dezhizhang <1541609448@qq.com>
2019-11-03 23:29:21 +0800

    提交

--- lib/Detail.dart
+++ lib/Detail.dart
@@ -8,13 +8,40 @@ class Detail extends StatelessWidget{
     // TODO: implement build
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text('详情页${this.arguments}'),
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text('详情页'),
+            bottom: TabBar(
+              tabs: <Widget>[
+                Tab(text: '热门'),
+                Tab(text: '推荐',)
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: <Widget>[
+              ListView(
+                children: <Widget>[
+                  ListTile(
+                    title: Text('热门'),
+                  )
+                ],
+              ),
+              ListView(
+                children: <Widget>[
+                  ListTile(
+                    title: Text('推荐'),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
-        body: Text('hello')
       ),
     );
   }
 }
+
