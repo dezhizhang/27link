e8b78e3b950a18555a8dcbdfd48cea3d54da9aa7
dezhizhang <1541609448@qq.com>
2019-11-06 00:17:48 +0800

    时间组件

--- lib/ThreeData.dart
+++ lib/ThreeData.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+
+import 'package:date_format/date_format.dart';
 import 'package:flutter_cupertino_date_picker/flutter_cupertino_date_picker.dart';
+
 
 
 class ThreeData extends StatefulWidget {
@@ -34,26 +37,35 @@ class HomeContent extends StatefulWidget {
 }
 
 class _HomeContentState extends State<HomeContent> {
-  var time = DateTime.now();
-  _showDatePicker() async {
-   DatePicker.showDatePicker(
-      context,
-      minDateTime: DateTime.parse('2019-10-12'),
-      maxDateTime: DateTime.parse('2021-12-10'),
-      initialDateTime: DateTime.parse('2019-12-10'),ss
-      dateFormat: 'yy年M月d日    EEE,H时:m分',
-      locale: DateTimePickerLocale.zh_cn,
-      pickerTheme: DateTimePickerTheme(
-        showTitle: true,
-      ),
-      pickerMode: DateTimePickerMode.datetime,
-      onChange: (dateTime, List<int> index) {
-        
-      },
-      onConfirm: (dateTime, List<int> index) {
-        
-      },
-    );
+ DateTime _dateTime=DateTime.now();
+
+  _showDatePicker(){
+      DatePicker.showDatePicker(
+          context,
+          pickerTheme: DateTimePickerTheme(
+            showTitle: true,
+            confirm: Text('确定', style: TextStyle(color: Colors.red)),
+            cancel: Text('取消', style: TextStyle(color: Colors.cyan)),
+          ),
+          minDateTime: DateTime.parse("1980-05-12"),
+          maxDateTime: DateTime.parse("2100-05-12"),
+          initialDateTime: _dateTime,
+          // dateFormat: "yyyy-MMMM-dd",
+
+          dateFormat:'yyyy年M月d日    EEE,H时:m分',  // show TimePicker
+          pickerMode: DateTimePickerMode.datetime,  // show TimePicker
+
+           
+          locale: DateTimePickerLocale.zh_cn,
+          onCancel: () {
+            debugPrint('onCancel');
+          },         
+          onConfirm: (dateTime, List<int> index) {
+            setState(() {
+              _dateTime = dateTime;
+            });
+          },
+      );  
   }
   @override
   Widget build(BuildContext context) {
